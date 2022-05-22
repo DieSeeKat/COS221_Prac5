02d0aaaf4f1f1be73342e168c51168052f0d8050
Lukas Anthonissen <u21434345@tuks.co.za>
2022-05-22 19:32:10 +0200

    config.php setup

--- php/config.php
+++ php/config.php
@@ -1,1 +1,50 @@
 <?php
+
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "";
+
+    readConfigFile("../config.json");
+
+    $GLOBALS["conn"] = new mysqli($servername, $username, $password, $dbname);
+
+    //Function to connect and send query and return results
+    function dbQuery($query){
+        $result = $GLOBALS["conn"]->query($query);
+
+        if ($result instanceof mysqli_result) {
+
+            $out_array = [];
+
+            while ($row = $result->fetch_assoc()) {
+
+                $out_array[] = $row;
+
+            }
+
+            return $out_array;
+
+        }else{
+            return $result;
+        }
+    }
+
+    function readConfigFile($filename){
+        if (file_exists($filename)) {
+            $strJSONFileContents = file_get_contents($filename);
+            $jsonArray = json_decode($strJSONFileContents, true);
+
+            global $servername;
+            global $username;
+            global $password;
+            global $dbname;
+
+            $servername = $jsonArray["servername"];
+            $username = $jsonArray["username"];
+            $password = $jsonArray["password"];
+            $dbname = $jsonArray["dbname"];
+        }else {
+            echo "Error: Config file not found...";
+        }
+    }
